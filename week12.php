9aea351a730f15193c2dcb7d1a08bb70771b5a40
Myeongju Hong <hmjpearl@gmail.com>
2022-05-23 22:08:51 +0900

    feat: Fix conflict codes

--- week12.php
+++ week12.php
@@ -10,16 +10,11 @@
         //쿠키 생성 예제
     
         $id = setcookie("userid", "hmj");
-<<<<<<< HEAD
         $name = setcookie("username", "홍명주", time()+60*60*24); 
-=======
-        $name = setcookie("username", "홍명주", time()+60*60); 
->>>>>>> main
         //쿠키 유효시간(현재시간함수+유효시간(초))
 
         if($id and $name) {
             echo "쿠키 userid와 username 생성 완료 <br>";
-<<<<<<< HEAD
             echo "쿠키 username은 1시간 지속<br>";
         }
 
@@ -32,9 +27,6 @@
             echo "username cookie value: ".$username."<br>";
         } else {
             echo "쿠키 생성 오류";
-=======
-            echo "쿠키 username은 1시간 지속<br>s";
->>>>>>> main
         }
 
     ?>
