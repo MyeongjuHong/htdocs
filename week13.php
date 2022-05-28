52e7b7fa695fd7205e01c06a7a59ba52421d688d
Myeongju Hong <hmjpearl@gmail.com>
2022-05-28 15:39:25 +0900

    feat: Add 13_2 code

--- week13.php
+++ week13.php
@@ -19,6 +19,19 @@
         echo $_SESSION['userid']."<br>";
         echo $_SESSION['username']."<br>";
 
+
+        //세션 사용 예제
+
+        $userid = $_SESSION["userid"];
+        $username = $_SESSION["username"];
+
     ?>
+
+    <h3>등록된 세션의 사용</h3>
+    <ul>
+        <li>등록된 세션 userid 값 : <?= $userid?></li>
+        <li>등록된 세션 username 값 : <?= $username?></li>
+    </ul>
+    
 </body>
 </html>
